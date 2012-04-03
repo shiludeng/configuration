8b53d5b6fbd555af067b76074f4022ce9f47c603
azure <azure@azure.(none)>
2012-04-03 21:29:37 +0800

    add the install scripts of taglist and winmanager both are plugins of vim

---
+++ vim/plugins/taglist/install.sh
@@ -0,0 +1,2 @@
+wget http://sourceforge.net/projects/vim-taglist/files/vim-taglist/4.5/taglist_45.zip
+unzip taglist_45.zip -d ~/.vim/
---
+++ vim/plugins/winmanager/install.sh
@@ -0,0 +1,2 @@
+wget -O winmanager.zip http://www.vim.org/scripts/download_script.php?src_id=754
+unzip winmanager.zip -d ~/.vim/
