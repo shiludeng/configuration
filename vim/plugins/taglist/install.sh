fa9a86d6ff210ff790c6579cee2a4c7c0b103490
azure <indigazure@gmail.com>
2012-04-03 21:44:57 +0800

    modify the existed install scripts to remove the installed file.

--- vim/plugins/taglist/install.sh
+++ vim/plugins/taglist/install.sh
@@ -1,2 +1,3 @@
 wget http://sourceforge.net/projects/vim-taglist/files/vim-taglist/4.5/taglist_45.zip
 unzip taglist_45.zip -d ~/.vim/
+rm -f taglist_45.zip
--- vim/plugins/winmanager/install.sh
+++ vim/plugins/winmanager/install.sh
@@ -1,2 +1,3 @@
 wget -O winmanager.zip http://www.vim.org/scripts/download_script.php?src_id=754
 unzip winmanager.zip -d ~/.vim/
+rm -f winmanager.zip
