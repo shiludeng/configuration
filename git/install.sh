3a4337ddf35fe9ed6175816bf982082f92e16a7a
azure <lvtonsmith@gmail.com>
2012-08-23 10:11:01 +0800

    add git configuration file rename tmux/config.sh to tmux/install.sh

---
+++ git/install.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# copy .gitconfig to HOME folder
+echo "copy .gitconfig to HOME folder"
+cp -f .gitconfig ~/
---
+++ tmux/install.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# copy .tmux.conf to HOME folder
+echo "copy .tmux.conf to HOME folder"
+cp -f .tmux.conf ~/.tmux.conf
