1603537b9d0d82b19086249dc49b083fd94170cf
azure <lvtonsmith@gmail.com>
2012-10-25 09:47:41 +0800

    update the comments of myrsync.sh.

--- scripts/myrsync.sh
+++ scripts/myrsync.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-# backup the content of '/home/azure/data/'(the tailing slash is neccesary)
+# backup the contents of '/home/azure/data/'(the trailing slash is necessary)
 # to '/media/B63E8C063E8BBDBD/'
 rsync -aSv /home/azure/data/ /media/B63E8C063E8BBDBD/ 
