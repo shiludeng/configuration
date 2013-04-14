f6260caf5d3d5643fd6f38d1c484b8360a4ae616
azure <lvtonsmith@gmail.com>
2013-04-14 14:39:50 +0800

    update the backup script: modify the backup directory because of different mount point.

--- scripts/myrsync.sh
+++ scripts/myrsync.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 # backup the contents of '/home/azure/data/'(the trailing slash is necessary)
 # to '/media/B63E8C063E8BBDBD/'
-rsync -aSv --delete /home/azure/data/ /media/B63E8C063E8BBDBD/ 
+rsync -aSv --delete /home/azure/data/ /media/azure/B63E8C063E8BBDBD/ 
