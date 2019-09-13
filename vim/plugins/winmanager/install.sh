fdce0a3e1029e3180039cb86232d331d1839a1a2
lvtonsmith <lvtonsmith@gmail.com>
2019-09-13 16:57:54 +0800

    remove unused plugins

--- vim/plugins/winmanager/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-wget -O winmanager.zip http://www.vim.org/scripts/download_script.php?src_id=754
-
-if [ ! -d ~/.vim]; then
-mkdir -p ~/.vim
-fi
-
-unzip winmanager.zip -d ~/.vim/
-rm -f winmanager.zip
